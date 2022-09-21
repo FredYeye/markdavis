cdacd58b040a89b7c5c3f3008cd55a899c87e0f8
Fredrik Sandkvist <fredrik.sandq@gmail.com>
2022-09-21 10:29:11 +0200

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-#[derive(PartialEq)]
-pub enum Version
-{
+pub enum Version {
     Us, Jp,
 }
 
-impl Version
-{
-    fn offset(&self) -> usize
-    {
-        match self
-        {
+impl Version {
+    fn offset(&self) -> usize {
+        match self {
             Version::Us => 0,
             Version::Jp => 1,
         }
@@ -18,8 +13,7 @@ impl Version
 
 const DAYS: usize = 7;
 
-fn main()
-{
+fn main() {
     let mut rom = Rom::default();
 
     rom.set_version(Version::Us);
@@ -31,20 +25,15 @@ fn main()
     rom.calculate_averages();
 }
 
-pub struct Rom
-{
+pub struct Rom {
     data: [Vec<u8>; 2],
     version: Version,
 }
 
-impl Default for Rom
-{
-    fn default() -> Self
-    {
-        Self
-        {
-            data:
-            [
+impl Default for Rom {
+    fn default() -> Self {
+        Self {
+            data: [
                 std::fs::read("Mark Davis' The Fishing Master (USA).sfc").expect("Mark Davis' The Fishing Master (USA).sfc not found!"),
                 std::fs::read("Oomono Black Bass Fishing - Jinzouko Hen (Japan).sfc").expect("Oomono Black Bass Fishing - Jinzouko Hen (Japan).sfc not found!"),
             ],
@@ -54,34 +43,26 @@ impl Default for Rom
     }
 }
 
-impl Rom
-{
-    fn get_u8(&self, offset: usize) -> u8
-    {
+impl Rom {
+    fn get_u8(&self, offset: usize) -> u8 {
         self.data[self.version.offset()][offset]
     }
 
-    fn make_u16(&self, offset: usize) -> u16 //special case with offset
-    {
-        u16::from_le_bytes
-        (
-            [
-                self.data[self.version.offset()][ offset | 0x10000     ],
-                self.data[self.version.offset()][(offset | 0x10000) + 1],
-            ]
-        )
+    fn make_u16(&self, offset: usize) -> u16 { //special case with offset
+        u16::from_le_bytes([
+            self.data[self.version.offset()][ offset | 0x10000     ],
+            self.data[self.version.offset()][(offset | 0x10000) + 1],
+        ])
     }
 
-    pub fn set_version(&mut self, version: Version)
-    {
+    pub fn set_version(&mut self, version: Version) {
         self.version = version;
     }
 
-    pub fn print_spots(&self)
-    {
-        if self.version == Version::Jp
-        {
-            todo!("jp not implemented for spots")
+    pub fn print_spots(&self) {
+        match self.version {
+            Version::Us => (),
+            Version::Jp => todo!("jp not implemented for spots"),
         }
 
         let offset = 0xD90B + 0 * 2;
@@ -90,60 +71,44 @@ impl Rom
 
         let mut print_string = String::new();
 
-        for area in 0 .. 31
-        {
+        for area in 0 .. 31 {
             print_string.push_str(&format!("Area {}\n", area + 1));
 
             let sub_area = self.make_u16(stage_offset + area * 2) as usize;
             let mut spot_offsets = Vec::new();
 
-            for x in 0 .. 16
-            {
+            for x in 0 .. 16 {
                 let ofs = self.make_u16(sub_area + x * 2) as usize;
-                if ofs > 0xD800
-                {
+                if ofs > 0xD800 {
                     spot_offsets.push(ofs);
-                }
-                else
-                {
+                } else {
                     break;
                 }
             };
 
             let mut current_spot = 0;
 
-            while current_spot < spot_offsets.len()
-            {
+            while current_spot < spot_offsets.len() {
                 let mut spot_start = spot_offsets[current_spot];
-                let spot_end = if current_spot != spot_offsets.len() - 1
-                {
+                let spot_end = if current_spot != spot_offsets.len() - 1 {
                     spot_offsets[current_spot + 1]
-                }
-                else
-                {
-                    if area != 30
-                    {
+                } else {
+                    if area != 30 {
                         self.make_u16(stage_offset + area * 2 + 2) as usize
-                    }
-                    else // special case for area 30
-                    {
+                    } else { // special case for area 30
                         spot_start + 3 * 2
                     }
                 };
 
                 print_string.push_str(&format!("Sub area {} | ", current_spot + 1));
 
-                loop
-                {
+                loop {
                     print_string.push_str(&format!("{}", self.make_u16(spot_start) >> 8));
                     spot_start += 2;
 
-                    if spot_start < spot_end
-                    {
+                    if spot_start < spot_end {
                         print_string.push_str(", ");
-                    }
-                    else
-                    {
+                    } else {
                         break
                     }
                 }
@@ -158,30 +123,25 @@ impl Rom
         std::fs::write("spots.txt", print_string).expect("unable to write file");
     }
 
-    pub fn print_bait_ratings(&self, fix_bug: bool)
-    {
-        if self.version == Version::Jp
-        {
-            todo!("jp not implemented for bait ratings") // in jp: get offsets for 7 days
+    pub fn print_bait_ratings(&self, fix_bug: bool) {
+        match self.version {
+            Version::Us => (),
+            Version::Jp => todo!("jp not implemented for spots"),
         }
 
         let offset = 0x1571C;
 
         let mut print_string = String::new();
 
-        for bait_offset in 0x71 ..= 0xB9
-        {
-            for x in 0 .. DAYS - 1
-            {
-                let multiplier = match fix_bug
-                {
+        for bait_offset in 0x71 ..= 0xB9 {
+            for x in 0 .. DAYS - 1 {
+                let multiplier = match fix_bug {
                     false => 6, // bugged, in-game version
                     true  => 7, // corrected version
                 };
                 print_string.push_str(&format!("{}", self.get_u8(offset + bait_offset * multiplier + x)));
 
-                if x != 5
-                {
+                if x != 5 {
                     print_string.push_str(", ");
                 }
             }
@@ -191,26 +151,21 @@ impl Rom
         std::fs::write("bait_ratings.txt", print_string).expect("unable to write file");
     }
 
-    fn calculate_averages(&self)
-    {
-        let get_weight_as_proper_dec = |offset|
-        {
+    fn calculate_averages(&self) {
+        let get_weight_as_proper_dec = |offset| {
             let hex_str = format!("{:04X}", self.make_u16(offset));
-            match hex_str.parse::<u16>()
-            {
+            match hex_str.parse::<u16>() {
                 Ok(i) => i,
                 Err(e) => panic!("failed to parse: {e}"),
             }
         };
 
         let mut weights = [[0; 10]; DAYS];
-        for d in 0 .. DAYS
-        {
+        for d in 0 .. DAYS {
             let base_offset = [0x17D2, 0x17CE];
             let offset = self.make_u16(base_offset[self.version.offset()] + d * 2) as usize;
 
-            for x in 0 .. weights.len()
-            {
+            for x in 0 .. weights.len() {
                 weights[d][x] = get_weight_as_proper_dec(offset + x * 2);
             }
         }
@@ -219,17 +174,14 @@ impl Rom
 
         let days = [1, 2, 2, 3, 3, 3, 4]; //unknown days in bonus
         let names = ["spring", "summer", "fall", "winter 1", "winter 2", "championship", "bonus"];
-        for x in 0 .. DAYS - 1 // todo: in jp, calc all stages
-        {
+        for x in 0 .. DAYS - 1 { // todo: in jp, calc all stages
             print_string.push_str(&format!("{:<12} | ", names[x]));
             print_string.push_str(&Rom::test_perm(weights[x], days[x]));
         }
 
         let mut filename = String::from("winning_averages");
-        filename.push_str
-        (
-            match self.version
-            {
+        filename.push_str(
+            match self.version {
                 Version::Us => "_us.txt",
                 Version::Jp => "_jp.txt",
             }
@@ -238,20 +190,16 @@ impl Rom
         std::fs::write(filename, print_string).expect("unable to write file");
     }
 
-    fn test_perm(weights: [u16; 10], days: usize) -> String
-    {
+    fn test_perm(weights: [u16; 10], days: usize) -> String {
         let mut perm = vec![0_u8; days];
 
         let mut list_of_best = Vec::new();
 
-        'outer: loop
-        {
+        'outer: loop {
             let mut sum_per_fisher = [0; 10];
 
-            for fisher in 0 .. sum_per_fisher.len()
-            {
-                for day in 0 .. days
-                {
+            for fisher in 0 .. sum_per_fisher.len() {
+                for day in 0 .. days {
                     let mut weight_rotate = weights;
                     weight_rotate.rotate_right(perm[day] as usize);
 
@@ -263,12 +211,9 @@ impl Rom
             list_of_best.push(best);
 
             perm[0] += 1;
-            for x in 0 .. perm.len()
-            {
-                if perm[x] == 8
-                {
-                    if x == perm.len() - 1
-                    {
+            for x in 0 .. perm.len() {
+                if perm[x] == 8 {
+                    if x == perm.len() - 1 {
                         break 'outer;
                     }
 
